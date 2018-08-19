480f6137bddd6be819139415ac3c866a4f54b107
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-08-19 21:49:55 +0200

    fixed mybar

--- .config/i3/mybar.py
+++ .config/i3/mybar.py
@@ -5,7 +5,44 @@ import json
 from datetime import datetime
 
 import sys
+import os
 
+
+
+
+from getpass import getuser
+def user():
+    d = {
+        'full_text': ' {}'.format(getuser())
+    }
+    return d
+
+
+from socket import gethostname
+def hostname():
+    d = {
+        'full_text': ' {}'.format(os.uname()[1])
+    }
+    return d
+
+
+from pathlib import Path
+def battery():
+    path = Path('/sys/class/power_supply/BAT0/uevent')
+    if path.is_file():
+
+        f = path.open()
+        battery_text = f.read()
+        battery_data = dict([row.split('=') for row in battery_text.strip().splitlines()])
+
+        float_hours_left = int(battery_data['POWER_SUPPLY_ENERGY_NOW']) / float(battery_data['POWER_SUPPLY_POWER_NOW'])
+        hours_left = int(float_hours_left)
+        minutes_left = int(float_hours_left * 60) % 60
+
+        d = {
+            'full_text': '\uf240 {}% {}:{}'.format(battery_data['POWER_SUPPLY_CAPACITY'], hours_left, minutes_left)
+        }
+        return d
 
 
 
@@ -13,12 +50,13 @@ def get_message():
 
 
     message = [
-        {'full_text': ' White Stripes - Seven nation army'},
-        {'full_text': ''},
-        {'full_text': ''},
-        {'full_text': ''},
-        {'full_text': ' user'},
-        {'full_text': ' battlestation'},
+        #{'full_text': ' White Stripes - Seven nation army'},
+        #{'full_text': ''},
+        #{'full_text': ''},
+        #{'full_text': ''},
+        user(),
+        hostname(),
+        battery(),
         {
         'full_text': datetime.now().strftime(' %a, %Y-%m-%d %H:%M:%S'),
         'instance': 'asd',
